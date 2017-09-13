d555a49e05fd0a671690e5792a71ca0da869af36
Aivils Štoss <aivils@ithouse.lv>
2017-09-13 14:57:57 +0300

    Analytic - tests

--- app/services/analytic_service/community_look_and_feel.rb
+++ app/services/analytic_service/community_look_and_feel.rb
@@ -1,8 +1,8 @@
 module AnalyticService
   class CommunityLookAndFeel < IncrementalProperties
 
-    def process(_community, params)
-      community = Community.find(_community.id)
+    def process(orig_community, params)
+      community = Community.find(orig_community.id)
       community.assign_attributes(params)
       properties[ADMIN_CHANGED_COVER_PHOTO] += 1 if community.cover_photo_file_name_changed?
       properties[ADMIN_CHANGED_COVER_PHOTO] += 1 if community.small_cover_photo_file_name_changed?
---
+++ spec/services/analytics_service/api/api_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe AnalyticService::API::Api do
+  let(:community) { FactoryGirl.create(:community) }
+  let(:person) do
+    person = FactoryGirl.create(:person, community: community, is_admin: true)
+    FactoryGirl.create(:community_membership, community: community, person: person, admin: true)
+    person
+  end
+
+  context 'intercom' do
+    before do
+      APP_CONFIG.admin_intercom_app_id = '123'
+      APP_CONFIG.admin_intercom_access_token = 'ABC'
+    end
+
+    it '#send_event' do
+      expect_any_instance_of(AnalyticService::API::Intercom).to receive(:event)
+      AnalyticService::API::Api.send_event(person, {event_name: 'logout'})
+    end
+
+    it '#send_incremental_properties' do
+      expect_any_instance_of(AnalyticService::API::Intercom).to receive(:update_user_incremental_properties)
+      AnalyticService::API::Api.send_incremental_properties(person, {property: 1})
+    end
+  end
+end
+
